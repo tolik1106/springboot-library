b5a5c360380d69e12e70973943ae92ae77a03fa0
tolik1106 <tolik1106@i.ua>
2019-09-25 10:12:45 +0300

    start new project

---
+++ src/main/java/com/zhitar/library/ZhitarLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.zhitar.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhitarLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhitarLibraryApplication.class, args);
+	}
+
+}
